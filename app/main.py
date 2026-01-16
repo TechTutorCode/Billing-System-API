d08f8df32e95e2b62e533ce5917250c80450a91d
TechTutorCode <programmingtutor.ke@gmail.com>
2026-01-16 16:51:40 +0300

    cors

--- app/main.py
+++ app/main.py
@@ -40,7 +40,20 @@ app = FastAPI(
     docs_url="/docs",
     redoc_url="/redoc"
 )
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ==========================
 # Exception handlers
